812f7be84eb8539e5ff01da510653c0be504d433
Zakariyae El Amri <1510zaelamri@student.epfc.eu>
2023-12-04 02:23:20 +0100

    fix(view_notes): gestion du style

--- view/view_notes.php
+++ view/view_notes.php
@@ -4,45 +4,40 @@
     <meta charset="UTF-8">
     <title>Notes</title>
     <!-- Bootstrap CSS -->
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <style>
-        /* Additional custom styles, if needed */
-        body {
-            background-color: #f8f9fa;
-        }
-        .note-card {
-            margin-bottom: 10px;
-        }
-        .note-title {
+        .card-header
+        {
+            background-color: #343a40;
             color: white;
         }
     </style>
 </head>
-<body>
+<body class="bg-dark text-white">
 <?php include('navbar.php'); ?>
 
-    <div class="container mt-5">
-        <h1 class="mb-4">My Notes</h1>
-        <?php if (!empty($notes)): ?>
-            <div class="row">
-                <?php foreach ($notes as $note): ?>
-                    <div class="col-md-4">
-                        <div class="card note-card">
-                            <div class="card-header bg-dark note-title">
-                                <?= htmlspecialchars($note->title) ?>
-                            </div>
-                            <div class="card-body">
-                                <p class="card-text"><?= nl2br(htmlspecialchars($note->content)) ?></p>
-                                <!-- Add other note details here -->
-                            </div>
+<div class="container mt-5">
+    <h1 class="mb-4">My Notes</h1>
+    <?php if (!empty($notes)): ?>
+        <div class="row">
+            <?php foreach ($notes as $note): ?>
+                <!-- Use 'col-6' for two cards per line on small devices and 'col-md-4' for three cards per line on medium devices and up -->
+                <div class="col-6 col-md-4 mb-3">
+                    <div class="card text-bg-dark" style="max-width: 18rem;">
+                        <div class="card-header"><?= htmlspecialchars($note->title) ?></div>
+                        <div class="card-body">
+                            <p class="card-text"><?= nl2br(htmlspecialchars($note->content)) ?></p>
+                            <!-- Add other note details here -->
                         </div>
                     </div>
-                <?php endforeach; ?>
-            </div>
-        <?php else: ?>
-            <p>No notes found.</p>
-        <?php endif; ?>
-    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    <?php else: ?>
+        <p>No notes found.</p>
+    <?php endif; ?>
+</div>
+
     <!-- Bootstrap JS, Popper.js, and jQuery -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.9.3/dist/umd/popper.min.js"></script>
