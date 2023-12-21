75036dcb60c469546ff4286da83aeb72dbf5b164
Zakariyae EL Amri <1510zaelamri@student.epfc.eu>
2023-12-21 16:49:49 +0100

    fix(open_notes) factorisation du code qui affiche le temps de création et d'edition d'une note dans le dossier utils

--- view/view_open_checklist_note.php
+++ view/view_open_checklist_note.php
@@ -10,42 +10,10 @@
 </head>
 <body>
     <div class="container mt-5">
-<?php
-// Assuming $note->created_at is a DateTime object
-$now = new DateTime();
-$interval = $now->diff($note->created_at);
 
-// Function to format the interval into a human-readable format
-function formatTimeDifference($dateTime) {
-    if ($dateTime === null) {
-        return null;
-    }
-    $now = new DateTime();
-    $interval = $now->diff($dateTime);
-
-    if ($interval->y > 0) {
-        return $interval->y . " year" . ($interval->y > 1 ? "s" : "");
-    } elseif ($interval->m > 0) {
-        return $interval->m . " month" . ($interval->m > 1 ? "s" : "");
-    } elseif ($interval->d > 0) {
-        return $interval->d . " day" . ($interval->d > 1 ? "s" : "");
-    } elseif ($interval->h > 0) {
-        return $interval->h . " hour" . ($interval->h > 1 ? "s" : "");
-    } elseif ($interval->i > 0) {
-        return $interval->i . " minute" . ($interval->i > 1 ? "s" : "");
-    } else {
-        return $interval->s . " second" . ($interval->s > 1 ? "s" : "");
-    }
-}
+    <?php include('utils/util_dates.php'); ?>
 
 
-// Display the time difference in italic
-$createdAtMessage = "<i>Created " . formatTimeDifference($note->created_at) . " ago</i>";
-$editedAtMessage = $note->edited_at ? "<i>Edited " . formatTimeDifference($note->edited_at) . " ago</i>" : "";
-
-echo "<p>$createdAtMessage $editedAtMessage</p>";
-
-?>
 <div class="container py-4">
     <form>
         <!-- Title Field -->
