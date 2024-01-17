154e690ff26e5a7d1c6236a06eb368ccf216c8e7
Ouss <1010ouelazab@student.epfc.eu>
2024-01-18 00:36:29 +0100

    feat(view_share): ajout de la vue share avec le petit message

---
+++ view/view_shares.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Shared Notes</title>
+    <!-- Bootstrap CSS and Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+</head>
+<style>
+    .bi-arrow-left {
+        font-size: 30px;
+        position: fixed;
+        top: 20px;
+        left: 40px;
+        /* Déplacer légèrement vers la droite */
+        z-index: 1000;
+        color: white;
+
+    }
+</style>
+
+<body>
+    <div>
+        <a href="javascript:history.back()" class="bi bi-arrow-left"></a>
+    </div>
+
+    <div class="container mt-5">
+        <h1 class="mb-4">Shares:</h1>
+        <p><i>This not is not shared yet</i></p>
+        <div class="list-group">
+
+            <!-- Bootstrap JS, Popper.js, and jQuery -->
+            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.9.3/dist/umd/popper.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
