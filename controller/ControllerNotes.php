b387a2f42164c73b236bac637f721d0bfe4e718f
Bourouis younes <1403yobourouis@student.epfc.eu>
2024-05-31 20:42:36 +0200

    Fix:Bug

--- controller/ControllerNotes.php
+++ controller/ControllerNotes.php
@@ -781,51 +781,48 @@ class ControllerNotes extends Controller
     }
     public function updateNotesOrderAndPinStatus() {
         if ($this->user_logged()) {
-       
-        try {
-            $user = $this->get_user_or_redirect();
-            $userId = $user->get_id();
-            $sortedNoteIds = $_POST['orderedIds'] ?? [];
-            $dropZone = $_POST['dropZone'];
-            
-            // Validez le dropZone pour être sûr qu'il contient des valeurs attendues
-            if ($dropZone !== "pinned-notes" && $dropZone !== "other-notes") {
-                http_response_code(400); // Bad Request
-                echo json_encode(['status' => 'error', 'message' => 'Invalid drop zone.']);
-                exit;
-            }
-        
-            if (empty($sortedNoteIds)) {
-                echo json_encode(['status' => 'error', 'message' => 'No notes to update.']);
-                exit;
-            }
-        
-            $initialWeight = max(Note::get_max_weight_pinned($userId), Note::get_highest_weight_by_owner($userId))+1;
-            foreach ($sortedNoteIds as $noteId) {
-                $note = Note::get_note_by_id($noteId);
-                if (!$note || $note->owner !== $userId) {
-                    continue; 
+            try {
+                $user = $this->get_user_or_redirect();
+                $userId = $user->get_id();
+                $sortedNoteIds = $_POST['orderedIds'] ?? [];
+                $dropZone = $_POST['dropZone'];
+    
+                if ($dropZone !== "pinned-notes" && $dropZone !== "other-notes") {
+                    http_response_code(400); // Bad Request
+                    echo json_encode(['status' => 'error', 'message' => 'Invalid drop zone.']);
+                    exit;
                 }
-        
-                
-                $note->weight = $initialWeight++;
-                $note->persist();
-        
-                if ($dropZone === "pinned-notes" && !$note->isPinned()) {
-                    $note->pin(); 
-                } elseif ($dropZone === "other-notes" && $note->isPinned()) {
-                    $note->unpin(); 
+    
+                if (empty($sortedNoteIds)) {
+                    echo json_encode(['status' => 'error', 'message' => 'No notes to update.']);
+                    exit;
                 }
+    
+                $initialWeight = max(Note::get_max_weight_pinned($userId), Note::get_highest_weight_by_owner($userId)) + 1;
+                foreach ($sortedNoteIds as $noteId) {
+                    $note = Note::get_note_by_id($noteId);
+                    if (!$note || $note->owner !== $userId) {
+                        continue; 
+                    }
+    
+                    $note->weight = $initialWeight++;
+                    $note->persist();
+    
+                    if ($dropZone === "pinned-notes" && !$note->isPinned()) {
+                        $note->pin(); 
+                    } elseif ($dropZone === "other-notes" && $note->isPinned()) {
+                        $note->unpin(); 
+                    }
+                }
+    
+                echo json_encode(['status' => 'success', 'message' => 'Notes updated successfully.']);
+            } catch (Exception $e) {
+                http_response_code(500);
+                echo json_encode(['status' => 'error', 'message' => 'Internal Server Error: ' . $e->getMessage()]);
             }
-        
-            echo json_encode(['status' => 'success', 'message' => 'Notes updated successfully.']);
-        } catch (Exception $e) {
-            http_response_code(500);
-            echo json_encode(['status' => 'error', 'message' => 'Internal Server Error: ' . $e->getMessage()]);
         }
     }
     
-}
 
 
 public function manage_labels(): void
